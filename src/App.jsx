e72b261eddb009bc96b7980237c6ffa4c08a8b1f
Dimz~ <rizkyalphazee@gmail.com>
2023-10-16 20:12:28 +0700

    Fix: Messy Layout

--- src/App.jsx
+++ src/App.jsx
@@ -10,12 +10,12 @@ const App = () => {
   }
   useEffect(() => getJoke(), [])
   return (
-    <main className='w-[100vw] h-[100vh] bg-[#E9B824] flex justify-center items-center relative'>
-      <div className='absolute top-[3rem] sm:top-[2rem] text-center font-bold font-pixely flex flex-col mb-[2rem]'>
-        <span className='text-[2.4rem] sm:text-[2.6rem] md:text-[2.8rem] lg:text-[3rem] xl:text-[3.2rem] mb-[-0.4rem]'>Selamat Datang</span>
+    <main className='w-[100vw] h-[100vh] bg-[#E9B824] flex flex-col justify-center items-center'>
+      <div className=' top-[3rem] sm:top-[2rem] text-center font-bold font-pixely flex flex-col mb-[2rem]'>
+        <span className='text-[2.4rem] sm:text-[2.6rem] md:text-[2.8rem] lg:text-[3rem] xl:text-[3.2rem] -mb-[1rem]'>Selamat Datang</span>
         <span className='text-[1.4rem]'>Di WarungJokes 👋</span>
+        <p className=" font-inter top-[9rem] font-thin text-center text-xs italic max-w-[25rem] mt-[0.5rem]">Random Jokes Generator yang isinya Jokes-Jokes Receh yang biasa dipake bapak bapak, selamat mencoba 👍</p>
       </div>
-      <p className="absolute font-inter top-[9rem] text-center text-xs italic max-w-[25rem]">Random Jokes Generator yang isinya Jokes-Jokes Receh yang biasa dipake bapak bapak, selamat mencoba 👍</p>
       <div className='w-[80vw] sm:w-[60vw] md:w-[55vw] lg:w-[50vw] xl:w-[40vw] bg-[#272829] flex flex-col items-center gap-7 p-6 sm:p-[2rem] text-white font-inter'>
         <img src={emoji} alt="emoji" className='w-[4rem]' />
         <p className='text-sm sm:text-sm md:text-base text-center'>{joke}</p>
